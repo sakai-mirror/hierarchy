eee68ddd077ab48a0a54755c5288a12d003bbae8
bkirschn@umich.edu <bkirschn@umich.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2014-02-13 20:02:28 +0000

    INFRSTR-255 migrated from https://source.sakaiproject.org/contrib/hierarchy/ (see contrib repo for svn log history)
    git-svn-id: https://source.sakaiproject.org/svn/hierarchy/branches/1.0.2@283600 66ffb92e-73f9-0310-93c1-f5514f145a0a

---
+++ impl/src/java/org/sakaiproject/hierarchy/dao/impl/HierarchyDaoImpl.java
@@ -0,0 +1,24 @@
+/******************************************************************************
+ * HierarchyDaoImpl.java - created by aaronz on Jun 30, 2007
+ * 
+ * Copyright (c) 2007 Centre for Academic Research in Educational Technologies
+ * Licensed under the Educational Community License version 1.0
+ * 
+ * A copy of the Educational Community License has been included in this 
+ * distribution and is available at: http://www.opensource.org/licenses/ecl1.php
+ * 
+ *****************************************************************************/
+
+package org.sakaiproject.hierarchy.dao.impl;
+
+import org.sakaiproject.genericdao.hibernate.HibernateCompleteGenericDao;
+import org.sakaiproject.hierarchy.dao.HierarchyDao;
+
+/**
+ * Implementation of DAO
+ * 
+ * @author Aaron Zeckoski (aaronz@vt.edu)
+ */
+public class HierarchyDaoImpl extends HibernateCompleteGenericDao implements HierarchyDao {
+
+}
