f9d6c143fa1c5eb1845c2fa03a333209824880d1
bkirschn@umich.edu <bkirschn@umich.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2014-02-13 20:02:28 +0000

    INFRSTR-255 migrated from https://source.sakaiproject.org/contrib/hierarchy/ (see contrib repo for svn log history)
    git-svn-id: https://source.sakaiproject.org/svn/hierarchy/tags/hierarchy-1.2.6@283600 66ffb92e-73f9-0310-93c1-f5514f145a0a

---
+++ impl/src/ddl/oracle/hierarchy.sql
@@ -0,0 +1,29 @@
+
+    create table HIERARCHY_NODE (
+        ID number(19,0) not null,
+        directParentIds varchar2(2000),
+        parentIds varchar2(4000),
+        directChildIds varchar2(2000),
+        childIds varchar2(4000),
+        primary key (ID)
+    );
+
+    create table HIERARCHY_NODE_META (
+        ID number(19,0) not null,
+        hierarchyId varchar2(255),
+        isRootNode number(1,0) not null,
+        ownerId varchar2(255),
+        title varchar2(255),
+        description clob,
+        permToken varchar2(255),
+        isDisabled number(1,0) not null,
+        primary key (ID)
+    );
+
+    create index HIERARCHY_PERMTOKEN on HIERARCHY_NODE_META (permToken);
+
+    create index HIERARCHY_HID on HIERARCHY_NODE_META (hierarchyId);
+
+    create sequence HIERARCHY_META_ID_SEQ;
+
+    create sequence HIERARCHY_NODE_ID_SEQ;
---
+++ impl/src/ddl/postgres/hierarchy.sql
@@ -0,0 +1,29 @@
+
+    create table HIERARCHY_NODE (
+        ID int8 not null,
+        directParentIds varchar(2000),
+        parentIds varchar(4000),
+        directChildIds varchar(2000),
+        childIds varchar(4000),
+        primary key (ID)
+    );
+
+    create table HIERARCHY_NODE_META (
+        ID int8 not null,
+        hierarchyId varchar(255),
+        isRootNode bool not null,
+        ownerId varchar(255),
+        title varchar(255),
+        description text,
+        permToken varchar(255),
+        isDisabled bool not null,
+        primary key (ID)
+    );
+
+    create index HIERARCHY_PERMTOKEN on HIERARCHY_NODE_META (permToken);
+
+    create index HIERARCHY_HID on HIERARCHY_NODE_META (hierarchyId);
+
+    create sequence HIERARCHY_META_ID_SEQ;
+
+    create sequence HIERARCHY_NODE_ID_SEQ;
